fd830439c41c730a6e8d0ea597ac6ad07cb54501
jpuptagrafft <46825889+jpuptagrafft@users.noreply.github.com>
2019-02-08 13:31:11 -0800

    Sqlite Intergation & the Beginning of Motivation Type Automation
    Intergated Sqlite into our Project for future database uses. Also started the process of Javascript Automation on the New Motivation Page. Still need to find a good way to format Dates.

--- src/app/new-motivation/new-motivation.page.ts
+++ src/app/new-motivation/new-motivation.page.ts
@@ -6,11 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-motivation.page.scss'],
 })
 export class NewMotivationPage implements OnInit {
-	enableAlarm: boolean;
-	now: any;
+	now: any; //Get Current date as String
+	nowNum: any; //Get Current date
+	selected: string; //Current Default Selected
+	name: string; //Name of reminder
+	remind: string; //How often to remind
+	firstRemind: any; //first Date/time for Reminder
+	dateHolder: any; //A holder of Dates
+	enableAlarm: boolean; //Enables last Date
 	constructor() { 
 		this.enableAlarm = false;
 		this.now = new Date();
+		this.nowNum = new Date(3600000);
 		this.now = this.now.toString();
 	}
 
@@ -21,28 +28,45 @@ export class NewMotivationPage implements OnInit {
 	getNow(){
 		return this.now.getDate;	
 	}
-/*	setDefaults(){
-		var e = document.getElementById("defaults");
-		switch(e.value){
+	setDefaults(){
+		var e = this.selected;
+		console.log(e);
+		switch(e){
 			case "water":
+				this.dateHolder = new Date(this.nowNum.getFullYear(), this.nowNum.getMonth()+1,
+				this.nowNum.getDate(), this.nowNum.getHours(), this.nowNum.getMinutes(), this.nowNum.getSeconds(),
+				this.nowNum.getMilliseconds());
+				
+				this.name = "Drink Water";
+				this.remind = "hour";
+				this.firstRemind = this.nowNum.toISOString();
+				console.log(this.firstRemind);
 				break;
 			case "clean":
+				this.name = "Clean House"
 				break;
 			case "dishes":
+				this.name = "Wash Dishes"
 				break;
 			case "workout":
+				this.name = "Go to the Gym"
 				break;
 			case "meeting":
+				this.name = "Meeting with Someone"
 				break;
 			case "med":
+				this.name = "Take Medication"
 				break;
 			case "remind":
+				this.name = "Set Reminder"
 				break;
 			case "food":
+				this.name = "Eat Something"
 				break;
 			case "zzz":
+				this.name = "Go to Sleep"
 				break;
 		}
 				
-		}*/
+		}
 	}
