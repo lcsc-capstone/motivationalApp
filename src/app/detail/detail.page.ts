9bbc62dc36365ea1b228ed8e6a9965d747ed59e8
theonepiecewondo <46974579+theonepiecewondo@users.noreply.github.com>
2019-02-20 11:12:13 -0800

    Much better implementation of the accordion list
    Still in need for major CSS and integration of storage. The basis for the implementation of storage should be there.

--- src/app/detail/detail.page.ts
+++ src/app/detail/detail.page.ts
@@ -7,13 +7,33 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./detail.page.scss'],
 })
 export class DetailPage implements OnInit {
-	items: any = [];
-	header: any = new Array(10);
-	expansion = true;
+	items: any; //The items array initiziler 
+	shownGroup = null; //Controlls the hidden / shown values for the div in html
 	constructor() { 
-		this.header = ["1","2","3","4","5","6","7","8","9","10","11"];
-		this.items = ["test","other test"]
+		this.items = [
+		{ header: "This is a test", panel: "You are a test"},
+		{ header: "This is another test", panel: "You are great :)"}
+		];
+		
 	}
   	ngOnInit() {
   	}
+
+	/*
+		used to keep track of what item has which information
+	*/
+
+	toggleGroup(group) {
+      		if (this.isGroupShown(group)) {
+          		this.shownGroup = null;
+      		} else {
+          		this.shownGroup = group;
+      		}
+  	};
+	/*
+		keeps track of whether or not a group is hidden
+	*/
+  	isGroupShown(group) {
+      		return this.shownGroup === group;
+  	};
 }
