720ee772ff5c055826aed30c534213e60b5d2a12
theonepiecewondo <46974579+theonepiecewondo@users.noreply.github.com>
2019-02-25 11:58:59 -0800

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,3 @@
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule {}
-=======
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
