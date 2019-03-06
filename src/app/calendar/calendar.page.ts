cbed32e98701990bc833867c04a225f36a15166b
theonepiecewondo <mdbaerlocher@lcmail.lcsc.edu>
2019-03-06 11:45:39 -0800

    Made cleaner code...

--- src/app/calendar/calendar.page.ts
+++ src/app/calendar/calendar.page.ts
@@ -11,6 +11,7 @@ export class CalendarPage implements OnInit {
 	daysInThisMonth: any;
 	daysInLastMonth: any;
 	daysInNextMonth: any;
+	WeekDay: any;
 	CurrentMonth: any;
 	CurrentYear: any;
 	CurrentDate: any;
@@ -25,31 +26,36 @@ export class CalendarPage implements OnInit {
 		this.daysInLastMonth = new Date(this.CurrentYear,this.CurrentMonth-1,0).getDate();
 		this.daysInNextMonth = new Date(this.CurrentYear,this.CurrentMonth+1,0).getDate();
 		this.Month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-		var WeekDay = new Date(this.CurrentYear,this.CurrentMonth,1).getDay();
+		this.WeekDay = new Date(this.CurrentYear,this.CurrentMonth,1).getDay();
 		this.days = new Array();
-		for(var i = this.daysInLastMonth-(WeekDay-1); i <= this.daysInLastMonth; i++){ //Obtains the last few days of last month to keep the calendar balanced
-			this.days.push(i);
-		}
-		for(var i = 1; i <= this.daysInThisMonth; i++){ //Populates the calendar with all the current days of the month
-  			this.days.push(i);
-  		}
-		for(var i = this.daysInThisMonth-(WeekDay-1); i <= this.daysInThisMonth; i++){ //Obtains the last few days of next month to keep calendar balanced
-			this.days.push(i);
-		}
-		for(var i = 31; i < this.days.length; i++){
-				this.days.pop();
-		}
+		this.getPreviousDaysOfMonth();
+		this.getDaysOfMonth();
+		this.getPreviousDaysOfNextMonth();
+		this.MonthCheck();
 		console.log(this.days);
 	}
   	ngOnInit() {
   	}
   	//potential functions for cleaner looking code? Eventually?
-  	/*getDaysOfMonth(){
-  		days: new Array();
-  		for(var i = 1; i < this.daysInThisMonth; i++){
+  	getPreviousDaysOfMonth(){
+  		for(var i = this.daysInLastMonth-(this.WeekDay-1); i <= this.daysInLastMonth; i++){ //Obtains the last few days of last month to keep the calendar balanced
+			this.days.push(i);
+		}
+  	}
+  	getDaysOfMonth(){
+  	for(var i = 1; i <= this.daysInThisMonth; i++){ //Populates the calendar with all the current days of the month
   			this.days.push(i);
   		}
-  		console.log(this.days);
-  		return this.days;
-  	}*/
+  	}
+
+  getPreviousDaysOfNextMonth(){
+  	for(var i = this.daysInThisMonth-(this.WeekDay-1); i <= this.daysInThisMonth; i++){ //Obtains the last few days of next month to keep calendar balanced
+			this.days.push(i);
+		}
+  }
+  MonthCheck(){
+  	for(var i = 31; i < this.days.length; i++){
+				this.days.pop();
+		}
+  }
 }
