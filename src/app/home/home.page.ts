e80ac8530a3b88b0f453bb632c95e9a3421c3498
jpuptagrafft <46825889+jpuptagrafft@users.noreply.github.com>
2019-03-06 11:32:52 -0800

    Home Page Style Basis at Final Stages!
    Home page design is Done for the Sunset Background! All that needs to be done now is implementing the Database Information, and then get the styles for the other backgrounds!

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component,  OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 	constructor() { }
-	
+	ngOnInit() {this.showTime();}
+	showTime(){
+			var date = new Date();
+			var h = date.getHours(); // 0 - 23
+			var m = date.getMinutes(); // 0 - 59
+			var session = "AM";
+			var hs;
+			var ms;
+			
+			if(h == 0){
+				h = 12;
+			}
+			
+			if(h > 12){
+				h = h - 12;
+				session = "PM";
+			}
+			if(h < 10){ 
+				hs = "0" + h.toString();
+			} else {
+				hs = h.toString();
+			}
+			if(m < 10){ 
+				ms = "0" + m.toString();
+			} else {
+				ms = m.toString();
+			}			
+			var time = hs + ":" + ms + " " + session;
+			document.getElementById("MyClockDisplay").innerText = time;
+			document.getElementById("MyClockDisplay").textContent = time;
+			
+			setInterval(this.showTime, 1000);
+			
+		}
+
 }
