af91e81c0be84d568cb8d4fe032ce4b61b9dcb1a
mrvaccaro <46974362+mrvaccaro@users.noreply.github.com>
2019-03-08 11:11:14 -0800

    Uploading works so far
    Memory configuration and service update.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { IonicStorageModule } from '@ionic/storage';
+import { StorageService } from './storage.service';
 
 @NgModule({
   declarations: [AppComponent],
@@ -22,6 +23,7 @@ import { IonicStorageModule } from '@ionic/storage';
   providers: [
     StatusBar,
     SplashScreen,
+	StorageService,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/storage.service.ts
+++ src/app/storage.service.ts
@@ -5,14 +5,27 @@ import{Injectable} from '@angular/core';
 const STORAGE_KEY = "StoredMotivations";
 
 @Injectable({
-    providedIn: 'root'
+	providedIn: 'root'
 })
 
 export class StorageService {
 
-    constructor(public storage: Storage) {}
+	constructor(public storage: Storage) {}
 
-    getAllStoredMotivations(){
-        return this.storage.get(STORAGE_KEY);
-    }
+	getAllStoredMotivations(){
+		console.log(this.storage.get(STORAGE_KEY));
+		return this.storage.get(STORAGE_KEY);
+	}
+
+	addMotivation( motivation: Motivation ){
+		return this.getAllStoredMotivations().then(data => {
+			if(data){
+				data.push(motivation);
+				return this.storage.set(STORAGE_KEY, data);
+			}else{
+				return this.storage.set(STORAGE_KEY, [motivation]);
+			}
+		});
+	}
+
 }
