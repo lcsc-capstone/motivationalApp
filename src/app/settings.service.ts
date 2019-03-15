2bb8cf76acd940270256700d299ee36e89efb909
Starspike7 <46974514+Starspike7@users.noreply.github.com>
2019-03-15 11:45:05 -0700

    Settings corrections

--- src/app/settings.service.ts
+++ src/app/settings.service.ts
@@ -5,13 +5,19 @@ import { Injectable } from '@angular/core';
 })
 export class SettingsService {
 	public darkmode:boolean;
+	public nukedata:boolean;
 
 	constructor() {
 		this.darkmode = false;
+		this.nukedata = false;
 
 	}
 	switch_darkmode() {
 		this.darkmode = !this.darkmode;
 		console.log(this.darkmode);
+	}
+	switch_nukedata(){
+		this.nukedata = !this.nukedata;
+		console.log(this.nukedata);
 	}
 }
