f976177a633f2b86340d49d52b248cbc22940b4f
theonepiecewondo <mdbaerlocher@lcmail.lcsc.edu>
2019-03-18 10:37:58 -0700

    Added the first part of getting database storage

--- src/app/detail/detail.page.ts
+++ src/app/detail/detail.page.ts
@@ -1,27 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { Motivation } from '../motivation.interface';
+import { StorageService } from '../storage.service';
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.page.html',
   styleUrls: ['./detail.page.scss'],
 })
 export class DetailPage implements OnInit {
-	
+	test: any;
 	items: any; //The items array initiziler 
 	shownGroup = null; //Controlls the hidden / shown values for the div in html
-	constructor() { 
-		this.items = [
-		{ header: "This is a test", panel: "You are a test"},
-		{ header: "This is another test", panel: "You are great :)"},
-		];
-		
+	constructor(private storage: StorageService) { 
+		//this.items = new Array();
+		this.storage.getAllStoredMotivations().then(data =>{
+			this.items = data;
+			console.log(this.items[0].name);
+		})
 	}
   	ngOnInit() {
   	}
-
-	/*
-		used to keep track of what item has which information
-	*/
 
 
 	toggleGroup(group) {
