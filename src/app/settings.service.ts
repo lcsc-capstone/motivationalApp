64eb8ed144dc42e17399005c2f8b246a94df1a8b
Starspike7 <46974514+Starspike7@users.noreply.github.com>
2019-04-12 12:04:32 -0700

    Minor edits to create a popup window

--- src/app/settings.service.ts
+++ src/app/settings.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { StorageService } from '../storage.service'
-import { Motivation } from '../motivation.interface'
+import { StorageService } from './storage.service';
+import { Motivation } from './motivation.interface';
 @Injectable({
   providedIn: 'root'
 })
@@ -8,20 +8,22 @@ export class SettingsService {
 	public darkmode:boolean;
 	public nukedata:boolean;
 
-	constructor() {
+	constructor(private storage: StorageService) {
 		this.darkmode = false;
 		this.nukedata = false;
 
 	}
+	//This switches the app to dark mode. James' front page is gonna make this a bitch.
 	switch_darkmode() {
 		this.darkmode = !this.darkmode;
 		console.log(this.darkmode);
 	}
+	//This deletes storage. If it's broke, it is Matt's fault.
 	switch_nukedata(){
 		this.nukedata = !this.nukedata;
-		console.log(this.nukedata)();
-		if (nukedata == true){
-			StorageService.clearStorage();
+		console.log(this.nukedata);
+		if (this.nukedata == true){
+			this.storage.clearStorage();
 			this.nukedata = !this.nukedata;
 		}
 	}
