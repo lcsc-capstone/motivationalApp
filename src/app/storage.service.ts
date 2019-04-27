41829f686c5ac57e21c3801697bb494123e6f687
jpuptagrafft <46825889+jpuptagrafft@users.noreply.github.com>
2019-04-27 10:01:31 -0700

    More Error Checking
    Attempted to re-wrtie the switch statement into a series of if/else statements, with the same result. The Errors are detailed in the comments of the code...

--- src/app/storage.service.ts
+++ src/app/storage.service.ts
@@ -81,13 +81,31 @@ export class StorageService {
 	/*daysInMonth(month,year) {
 		return new Date(year, month, 0).getDate();
 	}
+	
 	repeatMotivation(){
 		var newDate;
 		this.getAllStoredMotivations().then(data => {
 			for (var i = 0; i < data.length; i++){
 				if(new Date(data[i].firstDate) <= Date.now()){
 					//make variable for new time
-					switch(data[i].remind){
+					switch(data[i].remind){ //beginning of switch statement Errors:
+					//[ng] ERROR in src/app/storage.service.ts(119,95): error TS1005: ';' expected.
+					//[ng] src/app/storage.service.ts(122,7): error TS1068: Unexpected token. A constructor, method, accessor, or property was expected.
+					//[ng] src/app/storage.service.ts(124,21): error TS1005: ',' expected.
+					//[ng] src/app/storage.service.ts(124,31): error TS1005: ',' expected.
+					//[ng] src/app/storage.service.ts(124,61): error TS1005: ';' expected.
+					//[ng] src/app/storage.service.ts(128,7): error TS1128: Declaration or statement expected.
+					//[ng] src/app/storage.service.ts(128,18): error TS1005: ';' expected.
+					//[ng] src/app/storage.service.ts(131,7): error TS1128: Declaration or statement expected.
+					//[ng] src/app/storage.service.ts(131,14): error TS1128: Declaration or statement expected.
+					//[ng] src/app/storage.service.ts(133,6): error TS1128: Declaration or statement expected.
+					//[ng] src/app/storage.service.ts(181,5): error TS1128: Declaration or statement expected.
+					//[ng] src/app/storage.service.ts(182,4): error TS1128: Declaration or statement expected.
+					//[ng] src/app/storage.service.ts(183,3): error TS1128: Declaration or statement expected.
+					//[ng] src/app/storage.service.ts(184,2): error TS1128: Declaration or statement expected.
+					//[ng] src/app/storage.service.ts(185,27): error TS1005: ',' expected.
+					//[ng] src/app/storage.service.ts(185,40): error TS1005: ';' expected.
+					//[ng] src/app/storage.service.ts(198,1): error TS1128: Declaration or statement expected.
 						case "month":
 							newDate = new Date((((3600000 * 24) * this.getDaysinMonth()) + Date.now()) - ((new Date()).getTimezoneOffset() * 60000));
 							break;
@@ -120,8 +138,42 @@ export class StorageService {
 							break;
 						default:
 							break;
-					}	
-				
+					} //end of switch statement 
+				//Rewritten as a if/else statement; Seems to have the same result as the switch statement... Errors 
+				//[ng] ERROR in src/app/storage.service.ts(167,93): error TS1005: ';' expected.
+				//[ng] src/app/storage.service.ts(169,6): error TS1128: Declaration or statement expected.
+				//[ng] src/app/storage.service.ts(191,5): error TS1128: Declaration or statement expected.
+				//[ng] src/app/storage.service.ts(192,4): error TS1128: Declaration or statement expected.
+				//[ng] src/app/storage.service.ts(193,3): error TS1128: Declaration or statement expected.
+				//[ng] src/app/storage.service.ts(194,2): error TS1128: Declaration or statement expected.
+				//[ng] src/app/storage.service.ts(195,27): error TS1005: ',' expected.
+				//[ng] src/app/storage.service.ts(195,40): error TS1005: ';' expected.
+				//[ng] src/app/storage.service.ts(208,1): error TS1128: Declaration or statement expected.
+				if(data[i].remind == "month"){ 
+					
+					newDate = new Date((((3600000 * 24) * this.getDaysinMonth()) + Date.now()) - ((new Date()).getTimezoneOffset() * 60000));
+				}else if(data[i].remind == "week"){
+					newDate = new Date((((3600000 * 24) * 7) + Date.now()) - ((new Date()).getTimezoneOffset() * 60000));
+				}else if(data[i].remind == "day"){
+					newDate = new Date(((3600000 * 24) + Date.now()) - ((new Date()).getTimezoneOffset() * 60000));
+				}else if(data[i].remind == "trihour"){
+					newDate = new Date(((3600000  * 3)+ Date.now()) - ((new Date()).getTimezoneOffset() * 60000));
+					while(newDate.getHours >= 21 || newDate.getHours <= 6){ //21 and 6 should be replaced by more reasonable variables
+						newDate = new Date(((3600000 * 3)  + newDate) - ((new Date()).getTimezoneOffset() * 60000));
+					}
+				}else if(data[i].remind == "bihour"){
+					newDate = new Date(((3600000 * 2) + Date.now()) - ((new Date()).getTimezoneOffset() * 60000));
+					while(newDate.getHours >= 21 || newDate.getHours <= 6){ //21 and 6 should be replaced by more reasonable variables
+						newDate = new Date((3600000 * 2) + newDate) - (new Date().getTimezoneOffset() * 60000));
+					}
+				}else if(data[i].remind == "hour"){
+					newDate = new Date((3600000 + Date.now()) - ((new Date()).getTimezoneOffset() * 60000));
+					while(newDate.getHours >= 21 || newDate.getHours <= 6){ //21 and 6 should be replaced by more reasonable variables
+						newDate = new Date((3600000 + newDate) - ((new Date()).getTimezoneOffset() * 60000));
+					}
+				}else if(data[i].remind == "joke"){
+					newDate = new Date((5000 + Date.now()) - ((new Date()).getTimezoneOffset() * 60000));
+				} //End of if/else statement
 				//set data[i].firstDate to new variable(Needs to be the toISOString)
 				data[i].firstDate = newDate.toISOString();
 				this.storage.set(STORAGE_KEY,data);
@@ -136,11 +188,10 @@ export class StorageService {
 						sound: data[i].sound,
 						foreground: true
 					});
-				return 0;
 				}
 			}
 		}
-	} */
+	}*/
 	editMotivation(motivation: Motivation){
 			this.getAllStoredMotivations().then(data => {
 				for(var i = 0; i < data.length; i++){
